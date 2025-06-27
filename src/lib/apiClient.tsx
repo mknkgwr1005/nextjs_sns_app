36254175ab6a50af08e49b77005251150cda9af8
mknkgwr1005 <mknkgwr@yahoo.co.jp>
2025-06-27 13:13:26 +0900

    apiClientを修正

--- src/lib/apiClient.tsx
+++ src/lib/apiClient.tsx
@@ -1,11 +1,20 @@
 import axios from "axios";
 import { headers } from "next/headers";
 
-const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+const apiClient = axios.create(
+  process.env.ENVIROMENT === "PRODUCTION"
+    ? {
+        baseURL: process.env.NEXT_PUBLIC_API_URL,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    : {
+        baseURL: process.env.NEXT_PUBLIC_API_FOR_LOCAL,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+);
 
 export default apiClient;
