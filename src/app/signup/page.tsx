d98b3a7cf4d4043364cbde5157fb21e666b71b5d
mknkgwr1005 <mknkgwr@yahoo.co.jp>
2025-07-17 14:45:46 +0900

    新規登録ページのテストを実装

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -104,6 +104,7 @@ const Signup = () => {
               </div>
               <div className="mt-6">
                 <button
+                  aria-label="signup"
                   type="submit"
                   className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                   onClick={(e) => {
