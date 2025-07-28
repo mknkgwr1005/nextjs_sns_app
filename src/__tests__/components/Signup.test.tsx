aaa2b4d007bfa440acb03501d8cdd69abb1eb44b
mknkgwr <mknkgwr@yahoo.co.jp>
2025-07-28 10:59:30 +0900

    新規登録のテストを修正・npm install

--- src/__tests__/components/Signup.test.tsx
+++ src/__tests__/components/Signup.test.tsx
@@ -14,12 +14,13 @@ test("新規登録に必要なボタンがすべて表示される", () => {
   render(<Signup />);
 
   const buttons = screen.getAllByRole("button");
-  expect(buttons).toHaveLength(1);
+  expect(buttons).toHaveLength(2);
 
   const headers = screen.getAllByRole("heading");
   expect(headers).toHaveLength(1);
 
   expect(screen.getByRole("button", { name: /signup/i })).toBeInTheDocument();
+  expect(screen.getByRole("button", { name: /go-back/i })).toBeInTheDocument();
 
   expect(screen.getByLabelText("お名前")).toBeInTheDocument();
   expect(screen.getByLabelText("メールアドレス")).toBeInTheDocument();
