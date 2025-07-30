6a75dc6b495bbb70d86f946257694b89c666ec28
mknkgwr1005 <mknkgwr@yahoo.co.jp>
2025-07-30 09:31:00 +0900

    新規登録のテストを修正

--- src/__tests__/components/Signup.test.tsx
+++ src/__tests__/components/Signup.test.tsx
@@ -33,7 +33,86 @@ describe("新規登録ページ", () => {
       screen.getByRole("button", { name: /go-back/i })
     ).toBeInTheDocument();
 
-  expect(screen.getByLabelText("お名前")).toBeInTheDocument();
-  expect(screen.getByLabelText("メールアドレス")).toBeInTheDocument();
-  expect(screen.getByLabelText("パスワード")).toBeInTheDocument();
+    expect(screen.getByLabelText("お名前")).toBeInTheDocument();
+    expect(screen.getByLabelText("メールアドレス")).toBeInTheDocument();
+    expect(screen.getByLabelText("パスワード")).toBeInTheDocument();
+  });
+
+  it("新規登録が実行できる", async () => {
+    const signupBtn = screen.getByRole("button", { name: /signup/i });
+
+    await userEvent.click(signupBtn);
+
+    waitFor(() => {
+      expect(apiClient.post).toHaveBeenCalledWith("/auth/register", {
+        username: "Taro",
+        email: "test@example.com",
+        password: "password",
+      });
+    });
+
+    waitFor(() => {
+      expect(signupBtn).not.toBeInTheDocument();
+    });
+  });
+
+  it("空欄を入力したとき、エラーメッセージが表示される", async () => {
+    const signupBtn = screen.getByRole("button", { name: /signup/i });
+
+    await userEvent.click(signupBtn);
+
+    waitFor(() => {
+      expect(apiClient.post).toHaveBeenCalledWith("/auth/register", {
+        username: "",
+        email: "",
+        password: "",
+      });
+    });
+
+    waitFor(() => {
+      expect(screen.getAllByTestId("errorMsg")).toMatch(
+        "正しい値を入力してください。"
+      );
+    });
+  });
+
+  it("正しくないメールアドレスを入力したとき、エラーメッセージが表示される", async () => {
+    const signupBtn = screen.getByRole("button", { name: /signup/i });
+
+    await userEvent.click(signupBtn);
+
+    waitFor(() => {
+      expect(apiClient.post).toHaveBeenCalledWith("/auth/register", {
+        username: "testuser",
+        email: "test.com",
+        password: "testcom",
+      });
+    });
+
+    waitFor(() => {
+      expect(screen.getAllByTestId("errorMsg")).toMatch(
+        "入力されたメールアドレスの形式が正しくありません。"
+      );
+    });
+  });
+
+  it("正しくないパスワードを入力したとき、エラーメッセージが表示される", async () => {
+    const signupBtn = screen.getByRole("button", { name: /signup/i });
+
+    await userEvent.click(signupBtn);
+
+    waitFor(() => {
+      expect(apiClient.post).toHaveBeenCalledWith("/auth/register", {
+        username: "testuser",
+        email: "test@user.com",
+        password: "test",
+      });
+    });
+
+    waitFor(() => {
+      expect(screen.getAllByTestId("errorMsg")).toMatch(
+        "入力されたパスワードの形式が正しくありません。"
+      );
+    });
+  });
 });
